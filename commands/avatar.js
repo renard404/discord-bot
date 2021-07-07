71401289b5550d455038dc0f6029f52db008a669
renard404 <komalbhale007@gmail.com>
2021-07-07 12:17:20 +0530

    implemented basic commands for bot

---
+++ commands/avatar.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    description: 'Avatar!',
+    aliases: ['icon', 'pfp'],
+    execute(message, args) {
+        if (!message.mentions.users.size) {
+            const file = new Discord.MessageAttachment(message.author.displayAvatarURL({ format: "png", dynamic: true }));
+            console.log("file", file);
+            message.channel.send({ files: [file] });
+        }
+
+        if (message.mentions.users) {
+            const fileList = message.mentions.users.map(user => {
+                return new Discord.MessageAttachment(user.displayAvatarURL({ format: "png", dynamic: true }));
+            });
+
+            // send the entire array of strings as a message
+            // by default, discord.js will `.join()` the array with `\n`
+            message.channel.send(fileList);
+        }
+
+    },
+};
